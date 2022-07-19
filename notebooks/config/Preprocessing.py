96e52cf1ce1d2276351a2fff4914466b4d01a25f
thissop <600018686@fjuhsd.org>
2022-07-19 06:06:06 -0700

    yes!

--- notebooks/config/Preprocessing.py
+++ notebooks/config/Preprocessing.py
@@ -11,8 +11,8 @@ class Configuration:
     def __init__(self):
         # For reading the training areas and polygons
         self.training_base_dir = '/mnt/c/Users/Research/Documents/GitHub/africa-trees/SampleAnnotations' 
-        self.training_area_fn = ''
-        self.training_polygon_fn = ''
+        self.training_area_fn = 'area.gpkg' #'training_areas_example.shp'
+        self.training_polygon_fn = 'test.gpkg' #'training_polygons_example.shp'
 
         # For reading the VHR images
         self.bands = [0]
@@ -22,17 +22,17 @@ class Configuration:
         self.raw_pan_image_prefix = 'pan_SSA_32628_010_003_mosaic_2_3_mask'
 
         # For writing the extracted images and their corresponding annotations and boundary file
-        self.path_to_write = '/mnt/c/Users/Research/Documents/GitHub/africa-trees/SampleAnnotations'
+        self.path_to_write = '/mnt/c/Users/Research/Documents/GitHub/africa-trees/SampleResults-Preprocessing'
         self.show_boundaries_during_processing = False
         self.extracted_file_type = '.tif' # .png' 
         self.extracted_ndvi_filename = 'ndvi'
         self.extracted_pan_filename = 'pan'
-        self.extracted_annotation_filename = 'annotation'
-        self.extracted_boundary_filename = 'boundary'
+        self.extracted_annotation_filename = 'annotation' #annotation
+        self.extracted_boundary_filename = 'boundary' #boundary
         
 
         # Path to write should be a valid directory
-        #assert os.path.exists(self.path_to_write)
+        assert os.path.exists(self.path_to_write)
 
         if not len(os.listdir(self.path_to_write)) == 0:
             print('Warning: path_to_write is not empty! The old files in the directory may not be overwritten!!')
